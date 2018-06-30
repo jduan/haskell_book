6c29b66d74c756ae1dc6b18dc1e98e6b0e51fc35
Jingjing Duan <duanjingjing@gmail.com>
2018-06-29 23:57:50 -0700

    only incorrect guesses count towards the guess limit

--- hangman/src/Game.hs
+++ hangman/src/Game.hs
@@ -9,25 +9,26 @@ import Data.Maybe (isJust)
 import System.Exit (exitSuccess)
 
 -- String: the word we're guessing
--- [Maybe Char] the characters we've filled in so far
--- Char the letters we've guessed so far
+-- [Maybe Char]: the characters we've filled in so far
+-- String: the incorrected letters we've guessed so far
 data Puzzle =
   Puzzle String
          [Maybe Char]
          String
 
 instance Show Puzzle where
-  show (Puzzle _ discovered guessed) =
-    intersperse ' ' (fmap renderPuzzleChar discovered) ++ "  Guessed so far: " ++ guessed
+  show (Puzzle _ discovered incorrect) =
+    intersperse ' ' (fmap renderPuzzleChar discovered) ++
+    "  Incorrect guessed so far: " ++ incorrect
 
 -- Generate a fresh puzzle given a word
 -- "discovered" is initialized to a list of Nothing
--- "guessed" is initialized to an empty list
+-- "incorrect" is initialized to an empty list
 freshPuzzle :: String -> Puzzle
-freshPuzzle word = Puzzle word discovered guessed
+freshPuzzle word = Puzzle word discovered incorrect
   where
     discovered = map (const Nothing) word
-    guessed = []
+    incorrect = []
 
 -- Check if a char is in the puzzle or not
 charInWord :: Puzzle -> Char -> Bool
@@ -35,22 +36,26 @@ charInWord (Puzzle word _ _) char = char `elem` word
 
 -- Check if a char is already guessed or not
 alreadyGuessed :: Puzzle -> Char -> Bool
-alreadyGuessed (Puzzle _ _ guessed) char = char `elem` guessed
+alreadyGuessed (Puzzle _ _ incorrect) char = char `elem` incorrect
 
 -- Render a "char" in the discovered list of chars
 renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar Nothing = '_'
 renderPuzzleChar (Just char) = char
 
--- Given a char, fill it in the puzzle. Update "discovered" and "guessed"
+-- Given a char, fill it in the puzzle. Update "discovered" and "incorrect"
 fillInCharacter :: Puzzle -> Char -> Puzzle
-fillInCharacter (Puzzle word filledInSoFar s) c = Puzzle word newFilledInSoFar (c : s)
+fillInCharacter (Puzzle word filledInSoFar s) c = Puzzle word newFilledInSoFar newIncorrected
   where
     zipper wordChar guessChar =
       if wordChar == c
         then Just wordChar
         else guessChar
     newFilledInSoFar = zipWith zipper word filledInSoFar
+    newIncorrected =
+      if c `elem` word
+        then s
+        else c : s
 
 -- Ask the user for a char, and return a new puzzle.
 handleGuess :: Puzzle -> Char -> IO Puzzle
@@ -67,10 +72,13 @@ handleGuess puzzle guess = do
       putStrLn "This character wasn't in the word, try again."
       return (fillInCharacter puzzle guess)
 
+-- Max number of guesses allowed
+maxGuesses = 10
+
 -- Exit the game if too many guesses have been made
 gameOver :: Puzzle -> IO ()
-gameOver (Puzzle word _ guessed) =
-  when (length guessed > 7) $ do
+gameOver (Puzzle word _ incorrect) =
+  when (length incorrect > maxGuesses) $ do
     putStrLn "You lose!"
     putStrLn $ "The word was: " ++ word
     exitSuccess
@@ -86,8 +94,8 @@ gameWin (Puzzle _ filledInSoFar _) =
 runGame :: Puzzle -> IO ()
 runGame puzzle =
   forever $ do
-    gameOver puzzle
     gameWin puzzle
+    gameOver puzzle
     putStrLn $ "Current puzzle is: " ++ show puzzle
     putStr "Guess a letter: "
     guess <- getLine
