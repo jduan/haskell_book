a8afe7b7e3a7dec6e0096ab2e9373ba563d134b6
Jingjing Duan <duanjingjing@gmail.com>
2018-07-02 09:25:31 -0700

    finish chapter 6

--- chapter06/MatchTheTypes.hs
+++ chapter06/MatchTheTypes.hs
@@ -1,2 +1,33 @@
+import Data.List
+
 i :: Num a => a
 i = 1
+
+f :: RealFrac a => a
+f = 1.0
+
+frued :: Int -> Int
+frued x = x
+
+myX = 1 :: Int
+
+sigmund :: Int -> Int
+sigmund x = myX
+
+jung :: [Int] -> Int
+jung xs = head (sort xs)
+
+young :: Ord a => [a] -> a
+young xs = head (sort xs)
+
+mySort :: [Char] -> [Char]
+mySort = sort
+
+signifier :: [Char] -> Char
+signifier xs = head (mySort xs)
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f a b = f a == b
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith f i a = f a
