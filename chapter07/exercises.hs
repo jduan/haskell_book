27aba87a1b3b3e552f53fda9e64ec4ede185edb2
Jingjing Duan <duanjingjing@gmail.com>
2018-07-03 08:10:15 -0700

    Exercises: case practice

--- chapter07/exercises.hs
+++ chapter07/exercises.hs
@@ -24,3 +24,19 @@ addFive2 =
 mflip f = \x -> \y -> f y x
 
 mflip2 f x y = f y x
+
+functionC x y =
+  case x > y of
+    True -> x
+    False -> y
+
+ifEvenAdd2 n =
+  case even n of
+    True -> n + 2
+    False -> n
+
+nums x =
+  case compare x 0 of
+    LT -> -1
+    GT -> 1
+    EQ -> 0
