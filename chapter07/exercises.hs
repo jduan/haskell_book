575de2b309342e58ce6f265167251c603d268b83
Jingjing Duan <duanjingjing@gmail.com>
2018-07-04 11:48:52 -0700

    chapter exercises

--- chapter07/exercises.hs
+++ chapter07/exercises.hs
@@ -46,3 +46,37 @@ dodgy x y = x + y * 10
 oneIsOne = dodgy 1
 
 oneIsTwo = (flip dodgy) 2
+
+-- let's write code
+tensDigit :: Integral a => a -> a
+tensDigit x = d
+  where
+    (xLast, _) = x `divMod` 10
+    (_, d) = xLast `divMod` 10
+
+hundredsDigit :: Integral a => a -> a
+hundredsDigit x = d
+  where
+    (xLast, _) = x `divMod` 100
+    (_, d) = xLast `divMod` 10
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y b =
+  if b
+    then x
+    else y
+
+foldBool2 :: a -> a -> Bool -> a
+foldBool2 x y b =
+  case b of
+    True -> x
+    _ -> y
+
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (a, c) = (f a, c)
+
+roundTrip :: (Show a, Read b) => a -> b
+roundTrip x = read (show x)
+
+roundTripPF :: (Show a, Read a) => a -> a
+roundTripPF = read . show
