7f97b4a97ecd71e89e5e2d280192cfa14d0c3387
Jingjing Duan <duanjingjing@gmail.com>
2018-07-04 23:08:41 -0700

    fibonacci and dividedBy

--- chapter08/sandbox.hs
+++ chapter08/sandbox.hs
@@ -7,3 +7,28 @@ factorial n = n * factorial (n - 1)
 applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
 applyTimes 0 f b = b
 applyTimes n f b = f (applyTimes (n - 1) f b)
+
+-- bottom
+f :: Bool -> Int
+f True = error "blah"
+f False = 0
+
+-- f2 is a partial function (the opposite is a total function)
+f2 :: Bool -> Int
+f2 False = 0
+
+f3 :: Bool -> Maybe Int
+f3 False = Just 0
+f3 True = Nothing
+
+fibonacci :: Integral a => a -> a
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where
+    go n d count
+      | n < d = (count, n)
+      | otherwise = go (n - d) d (count + 1)
