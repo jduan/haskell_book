352a0b7adfe97daac22b800de301a5871080b5d1
Jingjing Duan <duanjingjing@gmail.com>
2018-07-07 11:12:51 -0700

    The Fearful Symmetry

--- chapter09/exercises.hs
+++ chapter09/exercises.hs
@@ -27,3 +27,34 @@ eftInt = eftGeneric
 
 eftChar :: Char -> Char -> String
 eftChar = eftGeneric
+
+-- The Fearful Symmetry
+--
+--
+-- Separate a sentence into words
+myWords :: String -> [String]
+myWords s = breakString s ' '
+
+-- Separate a string into a list o strings using a given delimiter.
+breakString :: String -> Char -> [String]
+breakString s delim = reverse $ go s []
+  where
+    go [] words = words
+    go s words = go (rest s) (firstWord s : words)
+    firstWord = takeWhile (/= delim)
+    removeLeadingSpaces = dropWhile (== delim)
+    rest s = removeLeadingSpaces $ dropWhile (/= delim) s
+
+-- Separate a multi-line string into a list of strings
+myLines :: String -> [String]
+myLines s = breakString s '\n'
+
+firstSen = "Tyger Tyger, burning bright\n"
+
+secondSen = "In the forests of the night\n"
+
+thirdSen = "What immortal hand or eye\n"
+
+fourthSen = "Could frame thy fearful symmetry?"
+
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
