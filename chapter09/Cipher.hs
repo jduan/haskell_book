546471a1a2f33ab41a5e3bb2192a2fbd57385adc
Jingjing Duan <duanjingjing@gmail.com>
2018-07-08 11:11:24 -0700

    unCaesar

--- chapter09/Cipher.hs
+++ chapter09/Cipher.hs
@@ -3,10 +3,16 @@ module Cipher where
 import Data.Char
 
 -- Encrypt a message by shifting each letter to the right
-caesar :: String -> Int -> String
-caesar message shift = map (shiftRight shift) message
+caesar :: Int -> String -> String
+caesar shift = map (shiftRight shift)
+
+-- Decrypt a message by shifting each letter to the left
+unCaesar :: Int -> String -> String
+unCaesar shift = map (shiftLeft shift)
 
 shiftRight distance letter
   | letter `elem` ['a' .. 'z'] = chr $ (ord letter - ord 'a' + distance) `mod` 26 + ord 'a'
   | letter `elem` ['A' .. 'Z'] = toUpper $ shiftRight distance (toLower letter)
   | otherwise = letter
+
+shiftLeft distance = shiftRight (negate distance)
