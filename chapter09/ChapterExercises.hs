f05fa1c43208f313cc8b87cfd79ccf1731f2a3f5
Jingjing Duan <duanjingjing@gmail.com>
2018-07-08 11:53:16 -0700

    Implement standard list functions

--- chapter09/ChapterExercises.hs
+++ chapter09/ChapterExercises.hs
@@ -19,3 +19,63 @@ upcase = map toUpper
 firstLetterUpcase :: String -> Maybe Char
 firstLetterUpcase [] = Nothing
 firstLetterUpcase xs = Just $ head $ capitalize xs
+
+-- Implement standard list functions
+--
+--
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) = x || myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) = f x || myAny f xs
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem x [] = False
+myElem x (y:ys) = x == y || myElem x ys
+
+myElem2 :: Eq a => a -> [a] -> Bool
+myElem2 x = myAny (== x)
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse xs = go xs []
+  where
+    go [] acc = acc
+    go (x:xs) acc = go xs (x : acc)
+
+squish :: [[a]] -> [a]
+squish [[]] = []
+squish ([]:xs) = squish xs
+squish ((y:ys):xs) = y : squish (ys : xs)
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f xs = squish $ map f xs
+
+squishAgain :: [[a]] -> [a]
+squishAgain xs = squishMap id xs
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f [x] = x
+myMaximumBy f (x:xs) =
+  let max = myMaximumBy f xs
+   in case f x max of
+        LT -> max
+        EQ -> max
+        GT -> x
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f [x] = x
+myMinimumBy f (x:xs) =
+  let max = myMinimumBy f xs
+   in case f x max of
+        LT -> x
+        EQ -> x
+        GT -> max
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum xs = myMaximumBy compare xs
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum xs = myMinimumBy compare xs
