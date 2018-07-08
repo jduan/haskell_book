b97e86cc0bc489a84cd4ef6e931daf436523193e
Jingjing Duan <duanjingjing@gmail.com>
2018-07-07 23:52:06 -0700

    Zipping Exercises

--- chapter09/exercises.hs
+++ chapter09/exercises.hs
@@ -91,3 +91,16 @@ myFilter :: String -> [String]
 myFilter sentence = dropArticles $ words sentence
   where
     dropArticles = filter (\x -> x `notElem` ["the", "a", "an"])
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = (x, y) : myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f [] _ = []
+myZipWith f _ [] = []
+myZipWith f (x:xs) (y:ys) = f x y : myZipWith f xs ys
+
+myZip2 :: [a] -> [b] -> [(a, b)]
+myZip2 = myZipWith (,)
