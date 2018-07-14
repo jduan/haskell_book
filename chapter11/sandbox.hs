35f467889539138babef67e8e2c572bc3b7f1526
Jingjing Duan <duanjingjing@gmail.com>
2018-07-13 22:56:03 -0700

    Vehicles

--- chapter11/sandbox.hs
+++ chapter11/sandbox.hs
@@ -36,3 +36,49 @@ data Doggies a
   = Husky a
   | Mastiff a
   deriving (Eq, Show)
+
+data Price =
+  Price Integer
+  deriving (Eq, Show)
+
+data Manufacturer
+  = Mini
+  | Mazda
+  | Tata
+  deriving (Show, Eq)
+
+data Airline
+  = PapuAir
+  | CapapultsR'Us
+  | TakeYourChancesUnited
+  deriving (Show, Eq)
+
+data Vehicle
+  = Car Manufacturer
+        Price
+  | Plane Airline
+          Integer
+  deriving (Show, Eq)
+
+myCar = Car Mini (Price 14000)
+
+urCar = Car Mazda (Price 20000)
+
+clownCar = Car Tata (Price 7000)
+
+doge = Plane PapuAir 99
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _ _) = True
+isPlane _ = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars = map isCar
+
+getManu :: Vehicle -> Manufacturer
+getManu (Car m _) = m
+getManu _ = error "not a car"
