3277e23a7bfcc222db24e5828719896de593b8b0
Jingjing Duan <duanjingjing@gmail.com>
2018-07-21 22:50:48 -0700

    Language Exercises

--- chapter11/AsPattern.hs
+++ chapter11/AsPattern.hs
@@ -1,19 +1,31 @@
 module AsPattern where
 
 import Data.Char
+import Data.List
+import Data.List.Split
 
-isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
-isSubsequenceOf [] _ = True
-isSubsequenceOf _ [] = False
-isSubsequenceOf left@(x:xs) right@(y:ys) =
+isSubsequenceOf' :: (Eq a) => [a] -> [a] -> Bool
+isSubsequenceOf' [] _ = True
+isSubsequenceOf' _ [] = False
+isSubsequenceOf' left@(x:xs) right@(y:ys) =
   if x == y
-    then isSubsequenceOf xs ys
+    then isSubsequenceOf' xs ys
     else let ys' = dropWhile (/= x) ys
-          in isSubsequenceOf left ys'
+          in isSubsequenceOf' left ys'
 
 capitalizeWords :: String -> [(String, String)]
 capitalizeWords str =
   let ws = words str
-   in zipWith (\w1 w2 -> (w1, capitalize w2)) ws ws
+   in zipWith (\w1 w2 -> (w1, capitalizeWord w2)) ws ws
+
+capitalizeWord :: String -> String
+capitalizeWord word = toUpper (head word) : tail word
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph parag = intercalate "." $ map capitalize parts
   where
-    capitalize word = toUpper (head word) : tail word
+    parts = splitOn "." parag
+        -- caplitalize first non white character
+    capitalize "" = ""
+    capitalize (' ':rest) = ' ' : capitalize rest
+    capitalize (x:xs) = toUpper x : xs
