4d840f384a66b0b52c6010aaa7a7b4f7b7d0127b
Jingjing Duan <duanjingjing@gmail.com>
2018-07-23 22:32:43 -0700

    Chapter 12

---
+++ chapter12/sandbox.hs
@@ -0,0 +1,19 @@
+ifEvenAdd2 :: Integer -> Maybe Integer
+ifEvenAdd2 n =
+  if even n
+    then Just (n + 2)
+    else Nothing
+
+type Name = String
+
+type Age = Integer
+
+data Person =
+  Person Name
+         Age
+  deriving (Show, Eq)
+
+mkPerson :: Name -> Age -> Maybe Person
+mkPerson name age
+  | name /= "" && age >= 0 = Just $ Person name age
+  | otherwise = Nothing
