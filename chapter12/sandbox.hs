6e3104213d76c2efd9af58318a2bfed108356f7c
Jingjing Duan <duanjingjing@gmail.com>
2018-07-23 22:49:15 -0700

    use Either types to model errors

--- chapter12/sandbox.hs
+++ chapter12/sandbox.hs
@@ -17,3 +17,33 @@ mkPerson :: Name -> Age -> Maybe Person
 mkPerson name age
   | name /= "" && age >= 0 = Just $ Person name age
   | otherwise = Nothing
+
+data PersonInvalid
+  = NameEmpty
+  | AgeTooLow
+  deriving (Show, Eq)
+
+mkPerson2 :: Name -> Age -> Either PersonInvalid Person
+mkPerson2 name age
+  | name == "" = Left NameEmpty
+  | age < 0 = Left AgeTooLow
+  | otherwise = Right $ Person name age
+
+-- checking functions
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age
+  | age >= 0 = Right age
+  | otherwise = Left [AgeTooLow]
+
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name
+  | name /= "" = Right name
+  | otherwise = Left [NameEmpty]
+
+mkPerson3 :: Name -> Age -> Either [PersonInvalid] Person
+mkPerson3 name age =
+  case (nameOkay name, ageOkay age) of
+    (Left errors, Left errors2) -> Left $ errors ++ errors2
+    (Left errors, Right age) -> Left errors
+    (Right name, Left errors) -> Left errors
+    (Right name, Right age) -> Right $ Person name age
