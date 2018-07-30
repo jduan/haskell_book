5d3ddddd4c822a676f0fecef19ca73e5a0243649
Jingjing Duan <duanjingjing@gmail.com>
2018-07-29 23:42:51 -0700

    Chapter Exercises

--- chapter15/exercises.hs
+++ chapter15/exercises.hs
@@ -1,4 +1,7 @@
-import Data.Monoid
+{-# LANGUAGE FlexibleInstances #-}
+
+import Data.Semigroup
+import Test.QuickCheck
 
 type Verb = String
 
@@ -29,3 +32,41 @@ madlibinBetter' e adv noun adj =
     , adj
     , " wife."
     ]
+
+--
+-- Chapter Exercises
+--
+-- Semigroup exercises
+--
+data Trivial =
+  Trivial
+  deriving (Show, Eq)
+
+instance Semigroup Trivial where
+  Trivial <> Trivial = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+newtype Identity a =
+  Identity a
+  deriving (Show, Eq)
+
+instance Semigroup a => Semigroup (Identity a) where
+  (Identity a) <> (Identity a') = Identity (a <> a')
+
+instance Arbitrary (Identity Trivial) where
+  arbitrary = return (Identity Trivial)
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+type IdentityAssoc
+   = Identity Trivial -> Identity Trivial -> Identity Trivial -> Bool
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (semigroupAssoc :: IdentityAssoc)
