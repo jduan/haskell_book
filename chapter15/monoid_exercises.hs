fcd11a72ef8956db70a5eadb06436ca1d306942f
Jingjing Duan <duanjingjing@gmail.com>
2018-08-01 22:32:23 -0700

    continue 'monoid exercises'

--- chapter15/monoid_exercises.hs
+++ chapter15/monoid_exercises.hs
@@ -68,8 +68,78 @@ newtype BoolConj =
   BoolConj Bool
   deriving (Show, Eq)
 
--- instance Monoid BoolConj where
---   mempty = 
+instance Monoid BoolConj where
+  mempty = BoolConj True
+  mappend (BoolConj True) (BoolConj True) = BoolConj True
+  mappend (BoolConj True) (BoolConj False) = BoolConj False
+  mappend (BoolConj False) (BoolConj True) = BoolConj False
+  mappend (BoolConj False) (BoolConj False) = BoolConj False
+
+type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+instance Arbitrary BoolConj where
+  arbitrary =
+    frequency [(1, return $ BoolConj True), (1, return $ BoolConj False)]
+
+newtype BoolDisj =
+  BoolDisj Bool
+  deriving (Show, Eq)
+
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+  mappend (BoolDisj True) (BoolDisj True) = BoolDisj True
+  mappend (BoolDisj True) (BoolDisj False) = BoolDisj True
+  mappend (BoolDisj False) (BoolDisj True) = BoolDisj True
+  mappend (BoolDisj False) (BoolDisj False) = BoolDisj False
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+instance Arbitrary BoolDisj where
+  arbitrary =
+    frequency [(1, return $ BoolDisj True), (1, return $ BoolDisj False)]
+
+newtype Combine a b = Combine
+  { unCombine :: a -> b
+  }
+
+instance Monoid b => Monoid (Combine a b) where
+  mempty = Combine $ const mempty
+  mappend (Combine f) (Combine g) = Combine (\a -> f a <> g a)
+
+combineF = Combine $ \n -> Sum (n + 1)
+
+combineG = Combine $ \n -> Sum (n - 1)
+
+newtype Comp a = Comp
+  { unComp :: a -> a
+  }
+
+instance Monoid a => Monoid (Comp a) where
+  mempty = Comp $ const mempty
+  mappend (Comp f) (Comp g) = Comp (\a -> f a <> g a)
+
+compF = Comp $ \n -> n ++ "!"
+
+compG = Comp $ \n -> n ++ "?"
+
+newtype Mem s a = Mem
+  { runMem :: s -> (a, s)
+  }
+
+instance Monoid a => Monoid (Mem s a) where
+  mempty = Mem $ \s -> (mempty, s)
+  mappend (Mem f) (Mem f') = Mem runMem
+    where
+      runMem s =
+        let (a, s1) = f s
+            (a', s2) = f' s
+            -- the trick is you apply f to s first, then apply f' to that
+            -- result
+            (_, s3) = f' s1
+         in (a <> a', s3)
+
+f' = Mem $ \s -> ("hi", s + 1)
+
 main :: IO ()
 main = do
   quickCheck (monoidAssoc :: TrivialAssoc)
@@ -81,3 +151,14 @@ main = do
   quickCheck (monoidAssoc :: TwoAssoc)
   quickCheck (monoidLeftIdentity :: Two String String -> Bool)
   quickCheck (monoidRightIdentity :: Two String String -> Bool)
+  quickCheck (monoidAssoc :: BoolConjAssoc)
+  quickCheck (monoidLeftIdentity :: BoolConj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolConj -> Bool)
+  quickCheck (monoidAssoc :: BoolDisjAssoc)
+  quickCheck (monoidLeftIdentity :: BoolDisj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolDisj -> Bool)
+  print $ runMem (f' <> mempty) 0
+  print $ runMem (mempty <> f') 0
+  print (runMem mempty 0 :: (String, Int))
+  print $ runMem (f' <> mempty) 0 == runMem f' 0
+  print $ runMem (mempty <> f') 0 == runMem f' 0
