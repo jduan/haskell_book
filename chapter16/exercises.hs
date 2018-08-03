2f80414e4f55faed05aa6ae19884c68c0d2c6900
Jingjing Duan <duanjingjing@gmail.com>
2018-08-02 23:50:21 -0700

    good and bad Functor instances

--- chapter16/exercises.hs
+++ chapter16/exercises.hs
@@ -32,3 +32,38 @@ data FixMePls a
 instance Functor FixMePls where
   fmap _ FixMe = FixMe
   fmap f (Pls a) = Pls $ f a
+
+data WhoCares a
+  = ItDoesnt
+  | Matter a
+  | WhatThisIsCalled
+  deriving (Show, Eq)
+
+--
+-- law-abiding instance
+-- instance Functor WhoCares where
+--   fmap _ ItDoesnt = ItDoesnt
+--   fmap _ WhatThisIsCalled = WhatThisIsCalled
+--   fmap f (Matter a) = Matter (f a)
+--
+--
+-- law-breaking instance because the "identity" law doesn't hold
+instance Functor WhoCares where
+  fmap _ ItDoesnt = WhatThisIsCalled
+  fmap f WhatThisIsCalled = ItDoesnt
+  fmap f (Matter a) = Matter (f a)
+
+data CountingBad a =
+  Heisenberg Int
+             a
+  deriving (Show, Eq)
+
+--
+-- doesn't comply with the composition law
+-- because "n" is changed.
+-- The fix is to stop messing with the Int. Think of anything that isn't
+-- the final type argument of our "f" in Functor as being part of the
+-- structure that the functions being lifted should be oblivious to.
+--
+instance Functor CountingBad where
+  fmap f (Heisenberg n a) = Heisenberg (n + 1) (f a)
