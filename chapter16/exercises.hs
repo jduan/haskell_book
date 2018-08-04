7f93609eac18f492170fbb9462d164aacb26bb7a
Jingjing Duan <duanjingjing@gmail.com>
2018-08-03 23:16:07 -0700

    Exercises: Heavy Lifting

--- chapter16/exercises.hs
+++ chapter16/exercises.hs
@@ -67,3 +67,18 @@ data CountingBad a =
 --
 instance Functor CountingBad where
   fmap f (Heisenberg n a) = Heisenberg (n + 1) (f a)
+--
+--
+-- Exercises: Heavy Lifting
+--
+-- 1. a = fmap (+1) $ read "[1]" :: [Int]
+--
+-- 2. b = (fmap . fmap) (++ "lol") (Just ["Hi,", "Hello"])
+--
+-- 3. c = (*2) . (\x -> x + 2)
+--
+-- 4. d = ((return '1' ++) . show) . (\x -> [x, 1..3])
+--
+-- 5. no idea
+--
+--
