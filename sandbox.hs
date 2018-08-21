bd886536e39e358f4c4eba8c38670c69142e0288
Jingjing Duan <duanjingjing@gmail.com>
2018-08-20 22:28:55 -0700

    update sandbox.hs

--- sandbox.hs
+++ sandbox.hs
@@ -1,19 +1,73 @@
 module Sandbox where
 
--- The output of skips is a list of lists. The first list in the output
--- should be the same as the input list. The second list in the output
--- should contain every second element from the input list. . . and the
--- nth list in the output should contain every nth element from the
--- input list.
---
--- For example:
--- given "hello"
--- it should return ["hello", "el", "l", "l", "o"]
-skips :: [a] -> [[a]]
-skips [] = [[]]
-skips xs = map (map snd) pairsFiltered
-  where
-    pairsFiltered = [filter (f nth) pairs | nth <- indices]
-    indices = enumFromTo 1 $ length xs
-    pairs = zip indices xs
-    f nth (index, x) = index `mod` nth == 0
+import Control.Applicative
+import Data.Char
+import Data.List
+
+-- Like Either!
+data Validation err a
+  = Failure err
+  | Success a
+  deriving (Show, Eq)
+
+data Errors
+  = DividedByZero
+  | StackOverflow
+  | MooglesChewedWires
+  deriving (Show, Eq)
+
+success :: Validation String Int
+success = Success (+ 1) <*> Success 1
+
+failure = Success (+ 1) <*> Failure [StackOverflow]
+
+failure' = Failure [StackOverflow] <*> Success (+ 1)
+
+failures = Failure [MooglesChewedWires] <*> Failure [StackOverflow]
+
+-- same as Either
+instance Functor (Validation e) where
+  fmap f (Failure e) = Failure e
+  fmap f (Success a) = Success (f a)
+
+-- Rather than just short-circuiting when it has 2 error values, it'll use
+-- the Monoid typeclass to combine them. Often this will just be a list or
+-- set of errors but you can do whatever you want.
+instance Monoid e => Applicative (Validation e) where
+  pure a = Success a
+  Success f <*> Success a = Success (f a)
+  Success f <*> Failure e = Failure e
+  Failure e <*> Success f = Failure e
+  Failure e <*> Failure e' = Failure (e `mappend` e')
+
+data CMaybe a
+  = CNothing
+  | CJust Int
+          a
+  deriving (Show, Eq)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust i a) = CJust (i + 1) (f a)
+
+apply :: Maybe (a -> b) -> Maybe a -> Maybe b
+apply Nothing _ = Nothing
+apply _ Nothing = Nothing
+apply (Just f) (Just a) = Just (f a)
+
+myAction :: IO String
+myAction = do
+  a <- getLine
+  b <- getLine
+  return $ a ++ b
+
+myAction' :: IO String
+myAction' = (++) <$> getLine <*> getLine
+
+sequenceA' :: (Applicative f) => [f a] -> f [a]
+sequenceA' [] = pure []
+sequenceA' (x:xs) = (:) <$> x <*> sequenceA' xs
+
+main = do
+  line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+  putStrLn line
