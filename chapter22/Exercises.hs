36ccf054eb738387dfcc8ee4a644ae814ec38583
Jingjing Duan <duanjingjing@gmail.com>
2018-09-01 22:10:50 -0700

    Exercises: Reading Comprehension

---
+++ chapter22/Exercises.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Exercises where
+
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 g f1 f2 = g <$> f1 <*> f2
+
+newtype Reader r a = Reader
+  { runReader :: r -> a
+  }
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
+
+instance Functor (Reader r) where
+  fmap f (Reader ra) = Reader (f . ra)
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader (const a)
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  (Reader rab) <*> (Reader ra) = Reader (\r -> rab r (ra r))
